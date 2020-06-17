56fe59b9d01de10c6aa0117ad6f69ed2333c2cf6
Mdsomratakbor <somrat876@gmail.com>
2020-06-17 20:54:28 +0600

    Book class add in Entities

---
+++ LibraryManagement.Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Entities
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public int BookName { get; set; }
+        public int Isbn { get; set; }
+        public string AuthorName { get; set; }
+        public string BookEdition { get; set; }
+        public DateTime EntryDate { get; set; }
+        public int LUserID { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public int UpdateLUserID { get; set; }
+    }
+}
