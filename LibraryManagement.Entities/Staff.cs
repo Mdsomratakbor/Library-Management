e9487721da1c3da69c3409c92100e818621e6aa7
Mdsomratakbor <somrat876@gmail.com>
2020-06-17 21:48:03 +0600

    Designation Entities Add

--- LibraryManagement.Entities/Staff.cs
+++ LibraryManagement.Entities/Staff.cs
@@ -11,6 +11,7 @@ namespace LibraryManagement.Entities
         public int ID { get; set; }
         public string Name { get; set; }
         public string DesignationID { get; set; }
+        public List<Designation> Designations { get; set; }
         public string Contact { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
