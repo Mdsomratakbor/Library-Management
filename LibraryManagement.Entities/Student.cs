a04456ce866c29710de941e90a87e2dbc0598229
Mdsomratakbor <somrat876@gmail.com>
2020-06-17 21:01:22 +0600

    student class some properties add

--- LibraryManagement.Entities/Student.cs
+++ LibraryManagement.Entities/Student.cs
@@ -10,7 +10,11 @@ namespace LibraryManagement.Entities
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Gender { get; set; }
+        public string Gender { get; set; }       
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
         public int DepartmentID { get; set; }
 
     }
