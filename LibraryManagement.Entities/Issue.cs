1622df729a652abcfdabcee4407bf839d2c62342
Mdsomratakbor <somrat876@gmail.com>
2020-06-17 20:57:38 +0600

    Issue Class add

---
+++ LibraryManagement.Entities/Issue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Entities
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        public int BookID { get; set; }
+        public List<Book> Books { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpiraryDate { get; set; }
+        public int StudentID { get; set; }
+    }
+}
