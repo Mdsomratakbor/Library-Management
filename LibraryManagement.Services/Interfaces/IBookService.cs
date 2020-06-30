8ac82066ad2afc9e911474107f1bb90aa347c264
Mdsomratakbor <somrat876@gmail.com>
2020-06-30 17:23:32 +0600

    IBookService Interface add

--- LibraryManagement.Services/Interfaces/IBookService.cs
+++ LibraryManagement.Services/Interfaces/IBookService.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagement.Services
+namespace LibraryManagement.Services.Interfaces
 {
-    public class BookService
+    interface IBookService
     {
-
     }
 }
