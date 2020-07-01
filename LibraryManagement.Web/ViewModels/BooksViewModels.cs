199d8fc1a9846f492edb01fb4037cec6c5382221
Mdsomratakbor <somrat876@gmail.com>
2020-07-01 12:56:50 +0600

    IBook Interface add

--- LibraryManagement.Web/ViewModels/BooksViewModels.cs
+++ LibraryManagement.Web/ViewModels/BooksViewModels.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using LibraryManagement.Entities;
+using LibraryManagement.Web.ViewModels.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace LibraryManagement.Web.ViewModels
 {
-    public class BooksViewModel
+    public class BooksViewModel: IBooks
     {
+        public List<Book> Books { get; set; }
     }
 }
---
+++ LibraryManagement.Web/ViewModels/Interfaces/IBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagement.Entities;
+
+namespace LibraryManagement.Web.ViewModels.Interfaces
+{
+    interface IBooks
+    {
+        List<Book> Books { get; set; }
+        
+    }
+}
