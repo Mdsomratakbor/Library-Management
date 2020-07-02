b64a41c08167c205b47f32a89eba43e9010ab13f
Mdsomratakbor <somrat876@gmail.com>
2020-07-02 17:47:46 +0600

    IBookList interface add

--- LibraryManagement.Web/ViewModels/BooksViewModels.cs
+++ LibraryManagement.Web/ViewModels/BooksViewModels.cs
@@ -7,12 +7,25 @@ using System.Web;
 
 namespace LibraryManagement.Web.ViewModels
 {
-    public class BooksViewModel: IBooks
+    public class BooksViewModel : IBookList
     {
         public List<Book> Books { get; set; }
     }
-    public class BookActionModel
+    public class BookActionModel : IBook
     {
-
+        public int ID { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public int? LUserID { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateLUserID { get; set; }
+        public string BookName { get; set; }
+        public int Isbn { get; set; }
+        public string AuthorName { get; set; }
+        public string BookPublish { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string BookEdition { get; set; }
+        public List<BookPicture> BookPictures { get; set; }
+        public int BookQty { get; set; }
     }
 }
--- LibraryManagement.Web/ViewModels/Interfaces/IBook.cs
+++ LibraryManagement.Web/ViewModels/Interfaces/IBook.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using LibraryManagement.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LibraryManagement.Entities;
 
 namespace LibraryManagement.Web.ViewModels.Interfaces
 {
-    interface IBooks
+    interface IBook: IBaseEntities
     {
-        List<Book> Books { get; set; }
-        
+         string BookName { get; set; }
+         int Isbn { get; set; }
+         string AuthorName { get; set; }
+         string BookPublish { get; set; }
+         DateTime PurchaseDate { get; set; }
+         decimal Price { get; set; }
+         string BookEdition { get; set; }
+         List<BookPicture> BookPictures { get; set; }
+        /// <summary>
+        /// How many book in this name available are library
+        /// </summary>
+         int BookQty { get; set; }
     }
 }
