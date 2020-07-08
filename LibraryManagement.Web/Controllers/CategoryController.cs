e43814ea3a916983afc12fc72958ad7827630081
Mdsomratakbor <somrat876@gmail.com>
2020-07-08 21:49:43 +0600

    category curde operation all method add in conroller

--- LibraryManagement.Web/Controllers/CategoryController.cs
+++ LibraryManagement.Web/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using LibraryManagement.Entities;
 using LibraryManagement.Services;
 using LibraryManagement.Services.Interfaces;
+using LibraryManagement.Web.ViewModels;
+using LibraryManagement.Web.ViewModels.CategoryInterfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +12,83 @@ using System.Web.Mvc;
 
 namespace LibraryManagement.Web.Controllers
 {
+    [HandleError]
     public class CategoryController : Controller
     {
         private ICategoryService _ICategoryService;
+        private ICategory _ICategory;
+        private Category _category;
         public CategoryController()
         {
             _ICategoryService = new CategoryServices();
+            _ICategory = new CategoryActionModel();
         }
         // GET: Category
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Action()
+        public async Task<ActionResult> Action(int? id)
         {
-            return View();
+            if (id > 0)
+            {
+                _category = await Task.Run(() => _ICategoryService.GetDataById(id.Value));
+                _ICategory.ID = _category.ID;
+                _ICategory.Name = _category.Name;
+                _ICategory.Description = _category.Description;
+            }
+            return View(_ICategory);
         }
+        [HttpPost]
+        public JsonResult Action(CategoryActionModel model)
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            var message = "";
+            bool isSuccess = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (model.ID > 0)
+                    {
+                        _category = _ICategoryService.GetDataById(model.ID);
+                        _category.Name = model.Name;
+                        _category.Description = model.Description;
+                        isSuccess = _ICategoryService.UpdateData(_category);
+                    }
+                    else
+                    {
+                        _category.ID = model.ID;
+                        _category.Name = model.Name;
+                        _category.Description = model.Description;
+                        isSuccess = _ICategoryService.SaveData(_category);
+                    }
+                }
+                else
+                {
+                    message = string.Join("; ", ModelState.Values
+                                           .SelectMany(x => x.Errors)
+                                           .Select(x => x.ErrorMessage));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            if (isSuccess)
+            {
+                message = "Data Save Successfully!!";
+                result.Data = new { Success = true, Message = message };
+            }
+            else
+            {
+                result.Data = new { Success = false, Message = message };
+            }
+            return result;
+        }
+
         public async Task<JsonResult> ListOfCategory(int iDisplayLength, int iDisplayStart, int iSortCol_0, string sSortDir_0, string sSearch)
         {
             int rowNumber;
--- LibraryManagement.Web/ViewModels/CategoryViewModels.cs
+++ LibraryManagement.Web/ViewModels/CategoryViewModels.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using LibraryManagement.Entities;
+using LibraryManagement.Web.ViewModels.CategoryInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace LibraryManagement.Web.ViewModels
 {
-    public class CategoryListModel
+    public class CategoryListModel : ICategoryList
     {
+        public List<Category> Categories { get; set; }
     }
-    public class CategoryActionModel
+    public class CategoryActionModel : ICategory
     {
-
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public int? LUserID { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateLUserID { get; set; }
     }
 }
