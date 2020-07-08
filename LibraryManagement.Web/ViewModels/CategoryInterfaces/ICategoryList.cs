2109eaec9b2f3cc7e8d0e02b79f5c557bf8405ce
Mdsomratakbor <somrat876@gmail.com>
2020-07-08 21:12:10 +0600

    Icategory list interface add

---
+++ LibraryManagement.Web/ViewModels/CategoryInterfaces/ICategoryList.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Web.ViewModels.CategoryInterfaces
+{
+    interface ICategoryList
+    {
+        List<Category> Categories { get; set; }
+    }
+}
