b2a8817948511759072514a56397cb1111732676
Mdsomratakbor <somrat876@gmail.com>
2020-07-10 20:05:57 +0600

    IDesignationList interface

--- LibraryManagement.Web/ViewModels/DesignationInterfaces/IDesignatioList.cs
+++ LibraryManagement.Web/ViewModels/DesignationInterfaces/IDesignatioList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Web.ViewModels.DesignationInterfaces
 {
-    interface IDesignation : IBaseEntties
+    interface IDesignatioList
     {
     }
 }
