231e8f3323a391570a91a9d86abc52048a1ef1c8
Mdsomratakbor <somrat876@gmail.com>
2020-07-10 18:15:44 +0600

    Departmentservices class IDerpartment services interface inherit

--- LibraryManagement.Services/DepartmentServices.cs
+++ LibraryManagement.Services/DepartmentServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagement.Entities;
+using LibraryManagement.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Services
 {
-    public class DepartmentServices
+    public class DepartmentServices : IDepartmentServices
     {
+        
+
+        public List<Department> GetAllData(int displayLength, int displayStart, int sortCol, string sortDir, string search = null)
+        {
+            throw new NotImplementedException();
+        }
+        public int TotalRowCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetDataById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveData(Department model)
+        {
+            throw new NotImplementedException();
+        }
+
+     
+
+        public bool UpdateData(Department model)
+        {
+            throw new NotImplementedException();
+        }
+        public bool DeleteData(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
