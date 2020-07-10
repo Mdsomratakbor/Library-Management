dff7c0528ef2f3099afd2867a85d431b90f3199e
Mdsomratakbor <somrat876@gmail.com>
2020-07-10 20:10:32 +0600

    DesignationViewModels class add

--- LibraryManagement.Web/ViewModels/DesignationInterfaces/IDesignation.cs
+++ LibraryManagement.Web/ViewModels/DesignationInterfaces/IDesignation.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using LibraryManagement.Web.ViewModels.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibraryManagement.Web.ViewModels.DesignationInterfaces
 {
-    interface IDesignation : IBaseEntties
+    interface IDesignation : IBaseEntities
     {
+        [Required (ErrorMessage="Please enter Designation")]
+        string Name { get; set; }
     }
 }
