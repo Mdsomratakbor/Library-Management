69c8ad734cca6c651173b96737ba2da2e9781990
Mdsomratakbor <somrat876@gmail.com>
2020-07-12 22:51:46 +0600

    role manager, usermanager, signmanager service class add

---
+++ LibraryManagement.Services/LMRolesManagerService.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagement.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+    public class LMRolesManagerService :RoleManager<IdentityRole>
+    {
+        public LMRolesManagerService(IRoleStore<IdentityRole, string> roleStore):base(roleStore)
+        {
+            
+        }
+        public static LMRolesManagerService Create(IdentityFactoryOptions<LMRolesManagerService>options, IOwinContext context)
+        {
+            return new LMRolesManagerService(new RoleStore<IdentityRole>(context.Get<LMContext>()));
+        }
+    }
+}
---
+++ LibraryManagement.Services/LMSignInManagerService.cs
@@ -0,0 +1,31 @@
+﻿using LibraryManagement.Entities;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+    public class LMSignInManagerService : SignInManager<LMUser, string>
+    {
+        public LMSignInManagerService(LMUserManagerService userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(LMUser user)
+        {
+            return user.GenerateUserIdentityAsync((LMUserManagerService)UserManager);
+        }
+
+        public static LMSignInManagerService Create(IdentityFactoryOptions<LMSignInManagerService> options, IOwinContext context)
+        {
+            return new LMSignInManagerService(context.GetUserManager<LMUserManagerService>(), context.Authentication);
+        }
+    }
+}
