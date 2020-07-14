d8f34b7901bf42948853a3e0a5a79e431b377081
Mdsomratakbor <somrat876@gmail.com>
2020-07-14 22:05:40 +0600

    IIssueServices interface add

---
+++ LibraryManagement.Services/Interfaces/IIssuServices.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Services.Interfaces
+{
+    public interface IIssuServices: IServices<Issue>
+    {
+    }
+}
