46508a1324ed4728d2a8cb40a3c3e3488bd84a40
Mdsomratakbor <somrat876@gmail.com>
2020-07-26 13:44:57 +0600

    database stratize add

--- LibraryManagement.Data/LibraryManagementDBInitializer.cs
+++ LibraryManagement.Data/LibraryManagementDBInitializer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LibraryManagement.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,5 +12,53 @@ namespace LibraryManagement.Data
 {
     public class LibraryManagementDBInitializer : CreateDatabaseIfNotExists<LMContext>
     {
+        protected override  void Seed(LMContext context)
+        {
+              SeedRoles(context);
+             SeedUser(context);
+        }
+
+        private  void SeedRoles(LMContext context)
+        {
+            var rolesInLibraryManagement = new List<IdentityRole>() 
+            { 
+                new IdentityRole() {Name="Admin" },
+                new IdentityRole() { Name = "Student" }, 
+                new IdentityRole() { Name = "User" }, 
+            };
+            var rolesStore = new RoleStore<IdentityRole>(context);
+            var rolesManager = new RoleManager<IdentityRole>(rolesStore);
+
+            foreach (IdentityRole role in rolesInLibraryManagement)
+            {
+                if (!rolesManager.RoleExists(role.Name))
+                {
+                    var result =  rolesManager.Create(role);
+
+                    if (result.Succeeded) continue;
+                }
+            }
+        }
+        private  void SeedUser(LMContext context)
+        {
+            var usersStore = new UserStore<LMUser>(context);
+            var usersManager = new UserManager<LMUser>(usersStore);
+            var user = new LMUser();
+            {
+                user.Email = "admin@mail.com";
+                user.UserName = "admin2";
+            }
+            var password = "admin1234";
+            if (usersManager.FindByEmailAsync(user.Email) == null)
+            {
+                var result =  usersManager.Create(user, password);
+
+                if (result.Succeeded)
+                {
+                    //add necessary roles to admin
+                    usersManager.AddToRole(user.Id, "Admin");
+                }
+            }
+        }
     }
 }
