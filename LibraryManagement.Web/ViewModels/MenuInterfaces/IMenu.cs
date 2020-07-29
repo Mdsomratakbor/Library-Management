c792048fc771d6aff7cb0550205901face570c84
Mdsomratakbor <somrat876@gmail.com>
2020-07-29 20:22:25 +0600

    add menuviewmodels class

--- LibraryManagement.Web/ViewModels/MenuInterfaces/IMenu.cs
+++ LibraryManagement.Web/ViewModels/MenuInterfaces/IMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManagement.Web.ViewModels.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Web.ViewModels.MenuInterfaces
 {
-    public interface IMenu
+    public interface IMenu : IBaseEntities
     {
+         string MenuName { get; set; }
+         string Controller { get; set; }
+         string Action { get; set; }
+         string ProjectName { get; set; }
+         int ParentId { get; set; }
+         bool IsParent { get; set; }
+         string Icon { get; set; }
     }
 }
--- LibraryManagement.Web/ViewModels/MenuInterfaces/IMenuList.cs
+++ LibraryManagement.Web/ViewModels/MenuInterfaces/IMenuList.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace LibraryManagement.Web.ViewModels.MenuInterfaces
 {
     public interface IMenuList
     {
+        List<Menu> Menus { get; set; }
     }
 }
