f8b3f4bda10e2b4c214e770807c58474948a6e0d
Mdsomratakbor <somrat876@gmail.com>
2020-07-29 20:00:50 +0600

    MenuController add

--- LibraryManagement.Services/MenuServices.cs
+++ LibraryManagement.Services/MenuServices.cs
@@ -1,4 +1,5 @@
-﻿using LibraryManagement.Entities;
+﻿using LibraryManagement.Data;
+using LibraryManagement.Entities;
 using LibraryManagement.Services.Abstractclass;
 using LibraryManagement.Services.Interfaces;
 using System;
@@ -11,9 +12,38 @@ namespace LibraryManagement.Services
 {
     public class MenuServices : LibraryManagementServices<Menu>, IMenuServices
     {
+        private LMContext _LMContext;
+        public MenuServices()
+        {
+            _LMContext = new LMContext();
+        }
         public List<Menu> GetAllData(int displayLength, int displayStart, int sortCol, string sortDir, string search = null)
         {
-            throw new NotImplementedException();
+            
+                string columnNameAsc = "";
+                string columnNameDsc = "";
+                List<Menu> menus = new List<Menu>();
+                if (sortCol == 0 && sortDir == "asc") columnNameAsc = "MenuName";
+                else if (sortCol == 0 && sortDir == "dsc") columnNameDsc = "MenuName";
+                if (sortDir == "asc" && string.IsNullOrEmpty(search) == false)
+                {
+                menus = _LMContext.Menus.OrderBy(x => columnNameAsc).Where(x => x.MenuName.ToString().ToLower().Contains(search.ToLower())).ToList();
+                }
+
+                else if (sortDir == "dsc" && string.IsNullOrEmpty(search) == false)
+                {
+                menus = _LMContext.Menus.OrderBy(x => columnNameDsc).Where(x => x.MenuName.ToString().ToLower().Contains(search.ToLower())).ToList();
+                }
+                else if (sortDir == "asc")
+                {
+                menus = _LMContext.Menus.OrderBy(x => columnNameAsc).ToList();
+                }
+                else
+                {
+                menus = _LMContext.Menus.OrderBy(x => columnNameDsc).ToList();
+                }
+                return menus;
+            
         }
     }
 }
