475731d71df209a9c1f6adbfc695607cb6885a63
Mdsomratakbor <somrat876@gmail.com>
2020-07-29 22:58:43 +0600

    add properties in roleviewmodels class

--- LibraryManagement.Web/ViewModels/MenuRoleViewModels.cs
+++ LibraryManagement.Web/ViewModels/MenuRoleViewModels.cs
@@ -1,6 +1,8 @@
 ﻿using LibraryManagement.Entities;
 using LibraryManagement.Web.ViewModels.MenuInterfaces;
 using LibraryManagement.Web.ViewModels.MenuRoleInterfaces;
+using LibraryManagement.Web.ViewModels.RoleInterfaces;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,19 @@ using System.Web;
 
 namespace LibraryManagement.Web.ViewModels
 {
-    public class MenuActionModel : IMenuRole, IMenuList, IRoleList
+    public class MenuRoleActionModel : IMenuRole, IMenuList, IRoleList
     {
-        public int RoleId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int MenuId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsUpdate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsDelete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsCreate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public List<Menu> Menus { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ID { get; set; }
+        public int RoleId { get; set; }
+        public int MenuId { get; set; }
+        public bool IsUpdate { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsCreate { get; set; }
+        public List<Menu> Menus { get; set; }
+        public List<IdentityRole> Roles { get; set; }     
+        public DateTime? EntryDate { get; set; }
+        public int? LUserID { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateLUserID { get; set; }
     }
 }
--- LibraryManagement.Web/ViewModels/RoleInterfaces/IRoleList.cs
+++ LibraryManagement.Web/ViewModels/RoleInterfaces/IRoleList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Web.ViewModels.RoleInterfaces
 {
-    interface IRoleList
+    public interface IRoleList
     {
+        List<IdentityRole> Roles { get; set; }
     }
 }
