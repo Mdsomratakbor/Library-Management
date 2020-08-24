6d9c09cabdaa15c9f1cf52fa6cc2f6cc76c1edaa
Mdsomratakbor <somrat876@gmail.com>
2020-08-25 05:56:40 +0600

    Home services some method add

--- LibraryManagement.Services/HomeServices.cs
+++ LibraryManagement.Services/HomeServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagement.Data;
+using LibraryManagement.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.Services
 {
-    public class HomeServices
+    public class HomeServices : IHomeServices
     {
+        private LMContext _context;
+        public HomeServices()
+        {
+            _context = new LMContext();
+        }
+        public int TotalBooks()
+        {
+           return  _context.Books.Count();
+            
+        }
+
+        public int TotalIssueBooks() 
+        {
+            return _context.Issues.Count();
+        }
+
+        public int TotalReturnBooks()
+        {
+            return _context.Returns.Count();
+        }
+
+        public int TotalRoles()
+        {
+           return _context.Roles.Count();
+        }
+
+        public int TotalStaffs()
+        {
+            return _context.Staffs.Count();
+        }
+
+        public int TotalStudents()
+        {
+            return _context.Students.Count();
+        }
+
+        public int TotalUsers()
+        {
+            return _context.Users.Count();
+        }
     }
 }
